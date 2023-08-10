783c5ffdf54d4c5cad3b6d53528f72ecd24dbc89
Andrey Horbunov <andrey.horbunov204@gmail.com>
2023-08-11 01:29:13 +0200

    Added enums

---
+++ NSmoke/Data/Static/Enums/Confidence.cs
@@ -0,0 +1,11 @@
+namespace NSmoke {
+    namespace Enums {
+        public enum Confidence {
+            NotSure,
+            RatherNotSure,
+            HardAnswer,
+            Sure,
+            VerySure
+        }
+    }
+}
---
+++ NSmoke/Data/Static/Enums/Mood.cs
@@ -0,0 +1,13 @@
+namespace NSmoke {
+    namespace Enums {
+        public enum Mood {
+            Happy,
+            Aggressive,
+            Calm,
+            Tired,
+            Inspired,
+            Bodny,
+            Wicked
+        }
+    }
+}
